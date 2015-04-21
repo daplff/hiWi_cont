3ccf04a393ad0055b32ce531ad95c5b1a15e7854
erik <daplff@gmail.com>
2015-04-21 18:43:21 +0200

    SimulationParameters has almost all paramters inside now

--- DataStructure/SimulationParameters.h
+++ DataStructure/SimulationParameters.h
@@ -13,6 +13,7 @@ public:
 	public:
 		double rho0;		//density of fluid
 		double viscos_val; 	//viscosity of fluid
+		double vlx, vly; 	//domain extent in x, y
 	} physical;
 	
 	struct Output {
@@ -28,21 +29,39 @@ public:
 			double tmax;	//end time
 			int ivar_dt;	//variable timesteps on/off (1=on)
 		} time;
-		struct ParticleInfo {
-			int np_b; 
-			//TODO
-		} particleInfo;
+		struct InitialParticles {
+			int np;			//initial no of fluid particles
+			int npv;		//initial no of virtual particles
+			int np_b;		//initial no of bed particles			
+		} initialParticles;
+		struct 
 		struct Refinement {
-			//TODO
+			double coef; 	//coefficient of init smoothing length to dx
+			double hsm_b_max;//smoothlength max for bottom particles
+			int n0;			// upper limit on number of particles per cell
+			double ref_p, ref_h; //coefficients of moved pos and smoothlen for split particles
+			double dw_min_ref;	//min elevation for allowing splitting
+			double xmin_ref, ymin_ref; // offsets of refinement grid origin to xpos and ypos
+			double dxx_ref, dyy_ref; // sizes of refinement grid
+			double ncx_ref, ncy_ref;	//number of cells in refinement grid
+			//TODO: implement refinement areas
 		} refinement;
 		
 		//variables concerning the newton-raphson procedure to find accelerations
 		//etc in gradient/density/pressures of particles
-		struct Interpolations 
-		{
-			//TODO
-		} interpolations;
-		
+		struct Technical {
+			int i_openbc; //number of open boundaries
+			double dw_min_fric; //minimum depth for friction source term
+			double distmin; //minimum distance between open boundary particles
+			double tol; 	//newton-raphson tolerance in acceleration calculations
+			int i_dw_iter;	//how often N-R iteration is made
+			int i_max_iter;	//max num of iterations
+			
+			int iMUSCL;		//muscl reconstruction (1=on) approximate fluxes at cell edges with interpolation over cell, supposedly more accurate (see wikipedia)
+			double CFL; 	//courant number, related to max dt/dx ratio for convergence of method (also see wikipedia)
+			
+			int i_restartrun; //restart an old run (1=yes)
+		}
 		
 	} simulation;
 };
